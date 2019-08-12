662b93b98fd28cb4b4b529195f4fc1b026326a4f
Tetiana Zakharova <kazakova.tanya@gmail.com>
2019-08-12 16:58:16 +0300

    Updates

---
+++ precache-manifest.6482fd8c27e3dd19f87eefb46c60d72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3340d135c2e70c7a52b2e25ab0b036b3",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "faaf6eba5ac2da4f2fdc",
+    "url": "/todo-list-react/static/css/main.8bc55e97.chunk.css"
+  },
+  {
+    "revision": "f3272dd5b6cf884288cb",
+    "url": "/todo-list-react/static/js/2.2e3a69aa.chunk.js"
+  },
+  {
+    "revision": "faaf6eba5ac2da4f2fdc",
+    "url": "/todo-list-react/static/js/main.06694590.chunk.js"
+  },
+  {
+    "revision": "967a5e11b9a4540eca48",
+    "url": "/todo-list-react/static/js/runtime~main.69a27755.js"
+  }
+]);
